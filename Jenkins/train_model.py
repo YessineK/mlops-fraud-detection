c89525e856dc75fa4fb3732c1d4d6c829aad0621
YessineK <119139575+YessineK@users.noreply.github.com>
2026-01-10 07:22:32 -1100

    Update train_model.py

--- Jenkins/train_model.py
+++ Jenkins/train_model.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
-from catboost import CatBoostClassifier
+#from catboost import CatBoostClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from dotenv import load_dotenv
 
